72e7c46603682132962f09a8ddeab11a48edab48
Dipto Biswas <diptobiswasanime4@gmail.com>
2025-03-04 01:21:31 +0530

    3

---
+++ dev/three/1 three/src/main.js
@@ -0,0 +1,38 @@
+import * as THREE from "three";
+
+const canvas = document.querySelector("canvas.webgl");
+
+console.log(THREE);
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 1;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+function animate() {
+  camera.position.x -= 0.1;
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
+}
+
+animate();
